d967da5cb363d70ef1b8cf02834317f81caf4e23
Lumworker <40013928+Lumworker@users.noreply.github.com>
2018-07-19 15:29:08 +0700

    no message

--- export.php
+++ export.php
@@ -7,16 +7,28 @@ $numberexport = isset($_REQUEST['numberexport']) ?$_REQUEST['numberexport'] :"";
 
 
 if($numberexport){
-    
- $datas = $database->select("data_tot", [
-    "number",
-    "name",
-    "location",
-    "promotion",
-], [
-    "number" => $numberexport,
+
+    $datas = $database->select("data_tot", [
+        "number",
+        "name",
+        "location",
+        "promotion",
+    ], [
+        "number" => $numberexport,
+    ]);
+}else{
+    $datas = $database->select("data_tot", [
+        "number",
+        "name",
+        "location",
+        "promotion",
     ]);
 }
-print_r($datas);
+//print_r($datas);
 
 ?>
+
+<?php foreach ($datas as $dat):?>
+    <button class="btn btn-success">name:<?php echo $dat["number"]?>number<?php echo $dat["name"]?></button>
+
+<?php endforeach;?>
